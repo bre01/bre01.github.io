5d75444967580f017e438e87abbf5f9dd6099694
bre <iwanttosignup321@gmail.com>
2023-12-01 18:28:31 +0800

    image optmization off

--- next.config.js
+++ next.config.js
@@ -5,12 +5,13 @@ const { withContentlayer } = require("next-contentlayer");
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
-let basePath = '/'
+let basePath = ''
 
 if (isGithubActions) {
   // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
+  
   assetPrefix = ``
   basePath = ``
 }
@@ -24,6 +25,9 @@ const nextConfig = {
   swcMinify: true,
   assetPrefix:assetPrefix,
   basePath:basePath,
+  images:{
+    unoptimized:true,
+  }
 };
 
 module.exports = withContentlayer(nextConfig);
