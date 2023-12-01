e48cf5a4524af8e85b5971ca88b39586a494d9bd
bre <iwanttosignup321@gmail.com>
2023-12-01 15:43:40 +0800

    deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,30 @@
 const { withContentlayer } = require("next-contentlayer");
+const repo='https://bre01.github.io'
+const myAssetPrefix=`/${repo}`;
+const myBasePath=`$/{repo}`
+
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix:assetPrefix,
+  basePath:basePath,
 };
 
 module.exports = withContentlayer(nextConfig);
